bff93d2a2d8257ba3a8939f8c7ad71463dea2d20
Heuristic Fencepost <h.fencepost@gmail.com>
2011-01-10 23:20:24 -0600

    Initial commit of Pike's C code.

---
+++ c/regex.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+/* Rob Pike's simple regex implementation from "The Practice of Programming".  Also
+   referenced by Brian Kerningham in the "Beautiful Code" book. */
+
+/* match: search for regexp anywhere in text */
+int match(char *regexp, char *text)
+{
+  if (regexp[0] == '^')
+    return matchhere(regexp+1, text);
+  do {    /* must look even if string is empty */
+    if (matchhere(regexp, text))
+      return 1;
+  } while (*text++ != '\0');
+  return 0;
+}
+
+/* matchhere: search for regexp at beginning of text */
+int matchhere(char *regexp, char *text)
+{
+  if (regexp[0] == '\0')
+    return 1;
+  if (regexp[1] == '*')
+    return matchstar(regexp[0], regexp+2, text);
+  if (regexp[0] == '$' && regexp[1] == '\0')
+    return *text == '\0';
+  if (*text!='\0' && (regexp[0]=='.' || regexp[0]==*text))
+    return matchhere(regexp+1, text+1);
+  return 0;
+}
+
+/* matchstar: search for c*regexp at beginning of text */
+int matchstar(int c, char *regexp, char *text)
+{
+  do {    /* a * matches zero or more instances */
+    if (matchhere(regexp, text))
+      return 1;
+  } while (*text != '\0' && (*text++ == c || c == '.'));
+  return 0;
+}
+
+int main()
+{
+  printf("^abc matching abc: %d\n",match("^abc","abc"));
+  printf("^abc matching ab: %d\n",match("^abc","ab"));
+  printf("abc matching fooabc: %d\n",match("abc","fooabc"));
+}
